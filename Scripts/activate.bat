1247c4d01ee70431b148069f8e6a8a31a8d919b7
RHA1705 <garbarzroman47@gmail.com>
2024-03-11 12:15:34 +0100

    new

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ROMAN\Repositories\GoIt-Core-002
+set VIRTUAL_ENV=C:\Users\ROMAN\Repositories\Final_project_Core
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(GoIt-Core-002) %PROMPT%
+set PROMPT=(Final_project_Core) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(GoIt-Core-002) 
+set VIRTUAL_ENV_PROMPT=(Final_project_Core) 
 
 :END
 if defined _OLD_CODEPAGE (
